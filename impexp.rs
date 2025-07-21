6cb92a7631cfcc027199c40423169097b37855e9
Rufus Raj <rufusrajdandangi@gmail.com>
2025-07-21 10:28:13 +0530

    Create impexp.rs

---
+++ impexp.rs
@@ -0,0 +1,10 @@
+fn impexp(){
+  let impint = 25;
+  let impchar = "Hello World!";
+  let expint: i64 = 27;
+  let expchar: f32 = "Programming";
+  println!("implicit int : {}",impint);
+  println!("implicit char : {}",impchar);
+  println!("explicit int(i64) : {}",expint);
+  println!("explicit char(f32) : {}",expchar);
+}
